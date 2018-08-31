dda9a9248b28db4545f11fa054de545b226c8dc5
Eric McIntyre <emcintyre@cyberscout.com>
2018-08-31 16:55:23 -0700

    Added functions to print database state at certain points
    Practiced reading rows, and also printing formatted strings

--- db_sql.go
+++ db_sql.go
@@ -27,6 +27,7 @@ import (
 	. "go-learn-sql/common"
 	"log"
 	"strings"
+	"time"
 )
 
 var db *sql.DB
@@ -40,7 +41,6 @@ func main() {
 		insertClient("Los Angeles Lakers"),
 		insertClient("Boston Celtics"),
 	}
-
 	customers := [8]Customer{
 		insertCustomer("123", "Kobe", "Bryant", "kbryant8@lakers.com", clients[0]),
 		insertCustomer("234", "Shaquille", "O'Neal", "soneal@lakers.com", clients[0]),
@@ -51,12 +51,12 @@ func main() {
 		insertCustomer("789", "Larry", "Bird", "lbird@celtics.com", clients[1]),
 		insertCustomer("890", "Paul", "Pierce", "ppierce@celtics.com", clients[1]),
 	}
-
 	products := [3]Product{
 		insertProduct("Super Personal Resolution Service"),
 		insertProduct("Fantastic Identity Monitoring"),
 		insertProduct("Watching Some Other Stuff"),
 	}
+	printDatabaseState()
 
 	updateCustomerName(customers[3], "Lew Alcindor")
 	updateProductName(products[2], "Stupendous Cyber Monitoring")
@@ -64,10 +64,12 @@ func main() {
 	deleteClient(clients[1])
 	updateClientName(clients[1], "Evil Empire")
 	deleteCustomer(customers[7])
+	printDatabaseState()
 
 	deleteAllCustomers()
 	deleteAllProducts()
 	deleteAllClients()
+	printDatabaseState()
 }
 
 func openDb() *sql.DB {
@@ -82,6 +84,141 @@ func openDb() *sql.DB {
 		log.Fatal(err)
 	}
 	return handle
+}
+
+func printDatabaseState() {
+	printClients()
+	printProducts()
+	printCustomers()
+	printCustomerProducts()
+}
+
+func printClients() {
+	log.Printf("*** %-15s ***", "Clients")
+	clients, err := db.Query("SELECT id, name, active, created_at, updated_at FROM client ORDER BY id")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer clients.Close()
+	var (
+		id        int64
+		name      string
+		active    bool
+		createdAt time.Time
+		updatedAt time.Time
+	)
+	log.Printf("%-3s | %-40s | %s | %-20s | %-20s", "ID", "Name", "Active", "Created At", "Updated At")
+	log.Println(strings.Repeat("-", 101))
+	rowCount := 0
+	for ; clients.Next(); rowCount++ {
+		err = clients.Scan(&id, &name, &active, &createdAt, &updatedAt)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("%-3d | %-40s | %-6t | %20s | %20s", id, name, active, createdAt.Format(time.RFC822), updatedAt.Format(time.RFC822))
+	}
+	log.Printf("Total: %d row(s)", rowCount)
+}
+
+func printProducts() {
+	log.Printf("*** %-15s ***", "Products")
+	products, err := db.Query("SELECT id, name, active, created_at, updated_at FROM product ORDER BY id")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer products.Close()
+	var (
+		id        int64
+		name      string
+		active    bool
+		createdAt time.Time
+		updatedAt time.Time
+	)
+	log.Printf("%-3s | %-40s | %s | %-20s | %-20s", "ID", "Name", "Active", "Created At", "Updated At")
+	log.Println(strings.Repeat("-", 101))
+	rowCount := 0
+	for ; products.Next(); rowCount++ {
+		err = products.Scan(&id, &name, &active, &createdAt, &updatedAt)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("%-3d | %-40s | %-6t | %20s | %20s", id, name, active, createdAt.Format(time.RFC822), updatedAt.Format(time.RFC822))
+	}
+	log.Printf("Total: %d row(s)", rowCount)
+}
+
+func printCustomers() {
+	log.Printf("*** %-15s ***", "Customers")
+	customers, err := db.Query(`
+		SELECT c.id, c.code, c.first_name, c.last_name, c.email_address, cl.name, c.created_at, c.updated_at
+		FROM customer c
+		JOIN client cl ON cl.id = c.client_id
+		ORDER BY c.id`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer customers.Close()
+	var (
+		id           int64
+		code         string
+		firstName    string
+		lastName     string
+		emailAddress string
+		clientName   string
+		createdAt    time.Time
+		updatedAt    time.Time
+	)
+	log.Printf("%-3s | %-10s | %-20s | %-20s | %-40s | %-40s | %20s | %20s",
+		"ID",
+		"Code",
+		"First Name",
+		"Last Name",
+		"Email",
+		"Client",
+		"Created At",
+		"Updated At")
+	log.Println(strings.Repeat("-", 194))
+	rowCount := 0
+	for ; customers.Next(); rowCount++ {
+		err = customers.Scan(&id, &code, &firstName, &lastName, &emailAddress, &clientName, &createdAt, &updatedAt)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("%-3d | %-10s | %-20s | %-20s | %-40s | %-40s | %20s | %20s",
+			id, code, firstName, lastName, emailAddress, clientName, createdAt.Format(time.RFC822), updatedAt.Format(time.RFC822))
+	}
+	log.Printf("Total: %d row(s)", rowCount)
+}
+
+func printCustomerProducts() {
+	log.Printf("*** %-15s ***", "Customer/Products")
+	customerProducts, err := db.Query(`
+		SELECT c.code, c.first_name, c.last_name, p.name
+		FROM customer c
+		INNER JOIN customer_product cp ON c.id = cp.customer_id
+		INNER JOIN product p ON cp.product_id = p.id
+		ORDER BY c.last_name`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer customerProducts.Close()
+	var (
+		code      string
+		firstName string
+		lastName  string
+		product   string
+	)
+	log.Printf("%-10s | %-20s | %-20s | %-40s", "Code", "First Name", "Last Name", "Product")
+	log.Println(strings.Repeat("-", 99))
+	rowCount := 0
+	for ; customerProducts.Next(); rowCount++ {
+		err = customerProducts.Scan(&code, &firstName, &lastName, &product)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("%-10s | %-20s | %-20s | %-40s", code, firstName, lastName, product)
+	}
+	log.Printf("Total: %d row(s)", rowCount)
 }
 
 func insertClient(name string) Client {
