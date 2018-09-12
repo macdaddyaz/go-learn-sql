06b0c0d3e2341ebc8219b986e4f4f07fd724b645
Eric McIntyre <emcintyre@cyberscout.com>
2018-09-12 15:18:25 -0700

    Finished implementing DAL in GORM
    Also refactored the GORM integration code to make the internal struct
    act as a "wrapper" around the GORM handle pointer.

--- gorm/operations.go
+++ gorm/operations.go
@@ -10,7 +10,7 @@ import (
 )
 
 type gormDao struct {
-	db *gorm.DB
+	*gorm.DB
 }
 
 func Init() gormDao {
@@ -25,21 +25,21 @@ func Init() gormDao {
 }
 
 func (dao gormDao) Shutdown() {
-	err := dao.db.Close()
+	err := dao.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
 func (dao gormDao) PrintDatabaseState() {
-	printClients(dao)
-	printProducts(dao)
-	printCustomers(dao)
+	dao.printClients()
+	dao.printProducts()
+	dao.printCustomers()
 }
-func printClients(dao gormDao) {
+func (dao gormDao) printClients() {
 	log.Printf("*** %-15s ***", "Clients")
 	var clients []Client
-	result := dao.db.Find(&clients)
+	result := dao.Find(&clients)
 	if result.Error != nil {
 		log.Fatal(result.Error)
 	}
@@ -56,10 +56,10 @@ func printClients(dao gormDao) {
 	log.Printf("Total: %d row(s)", len(clients))
 }
 
-func printProducts(dao gormDao) {
+func (dao gormDao) printProducts() {
 	log.Printf("*** %-15s ***", "Products")
 	var products []Product
-	result := dao.db.Find(&products)
+	result := dao.Find(&products)
 	if result.Error != nil {
 		log.Fatal(result.Error)
 	}
@@ -76,7 +76,7 @@ func printProducts(dao gormDao) {
 	log.Printf("Total: %d row(s)", len(products))
 }
 
-func printCustomers(dao gormDao) {
+func (dao gormDao) printCustomers() {
 	log.Printf("*** %-15s ***", "Customers")
 	log.Printf("%-3s | %-10s | %-20s | %-20s | %-40s | %-40s | %20s | %20s",
 		"ID",
@@ -89,7 +89,7 @@ func printCustomers(dao gormDao) {
 		"Updated At")
 	log.Println(strings.Repeat("-", 194))
 	var customers []Customer
-	result := dao.db.Find(&customers)
+	result := dao.Preload("Products").Find(&customers)
 	if result.Error != nil {
 		log.Fatal(result.Error)
 	}
@@ -105,7 +105,18 @@ func printCustomers(dao gormDao) {
 			customer.UpdatedAt.Format(time.RFC822))
 	}
 	log.Printf("Total: %d row(s)", len(customers))
-	// TODO Load product relationship and print
+	// Customer/Product relationship
+	log.Printf("*** %-15s ***", "Customer/Products")
+	log.Printf("%-10s | %-20s | %-20s | %-40s", "Code", "First Name", "Last Name", "Product")
+	log.Println(strings.Repeat("-", 99))
+	var rowCount int
+	for _, customer := range customers {
+		for _, product := range customer.Products {
+			rowCount++
+			log.Printf("%-10s | %-20s | %-20s | %-40s", customer.Code, customer.FirstName, customer.LastName, product.Name)
+		}
+	}
+	log.Printf("Total: %d row(s)", rowCount)
 }
 
 func (dao gormDao) InsertClient(name string) Client {
@@ -114,7 +125,7 @@ func (dao gormDao) InsertClient(name string) Client {
 		Name:   name,
 		Active: true,
 	}
-	result := dao.db.Create(&client)
+	result := dao.Create(&client)
 	if result.Error != nil {
 		log.Fatal(result.Error)
 	}
@@ -123,63 +134,116 @@ func (dao gormDao) InsertClient(name string) Client {
 
 func (dao gormDao) InsertCustomer(code, firstName string, lastName string, email string, client Client) Customer {
 	log.Println("Insert customer", firstName, lastName)
-	return Customer{}
+	customer := Customer{
+		Code:         code,
+		FirstName:    firstName,
+		LastName:     lastName,
+		EmailAddress: email,
+		Client:       client,
+	}
+	result := dao.Create(&customer)
+	if result.Error != nil {
+		log.Fatal(result.Error)
+	}
+	return customer
 }
 
 func (dao gormDao) InsertProduct(name string) Product {
 	log.Println("Insert product", name)
-	return Product{}
+	product := Product{
+		Name: name,
+	}
+	result := dao.Create(&product)
+	if result.Error != nil {
+		log.Fatal(result.Error)
+	}
+	return product
 }
 
 func (dao gormDao) UpdateCustomerName(customer Customer, newFullName string) {
 	log.Println("Update customer", customer.Id, "name to", newFullName)
-	// newFirstName, newLastName, err := SplitFullName(newFullName)
-	// logAffectedRows("Update customer name", res)
+	newFirstName, newLastName, err := SplitFullName(newFullName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	customer.FirstName = newFirstName
+	customer.LastName = newLastName
+	result := dao.Save(&customer)
+	if result.Error != nil {
+		log.Fatal(result.Error)
+	}
+	logAffectedRows("Update customer name", result)
 }
 
 func (dao gormDao) UpdateProductName(product Product, newName string) {
 	log.Println("Update product", product.Id, "name to", newName)
-	// logAffectedRows("Update product name", res)
+	product.Name = newName
+	result := dao.Save(&product)
+	if result.Error != nil {
+		log.Fatal(result.Error)
+	}
+	logAffectedRows("Update product name", result)
 }
 
 func (dao gormDao) UpdateCustomerEmailAndLinkToProduct(customer Customer, newEmail string, product Product) {
 	log.Println("Update customer", customer.Id, "email address to", newEmail)
-	// logAffectedRows("Update customer email", res)
+	customer.EmailAddress = newEmail
 	log.Println("Link product", product.Id, "to customer", customer.Id)
-	// logAffectedRows("Link customer to product", res)
+	customer.Products = append(customer.Products, product)
+	result := dao.Save(&customer)
+	if result.Error != nil {
+		log.Fatal(result.Error)
+	}
+	logAffectedRows("Update customer email and link to product", result)
 }
 
 func (dao gormDao) DeleteClient(client Client) {
 	log.Println("Delete client", client.Id)
+	result := dao.Delete(&client)
+	if result.Error == nil {
+		log.Fatal("Delete client was not blocked by DB constraints")
+	}
+	// log.Println(result.Error)
 	log.Println("Delete client was blocked by DB contraints, as expected")
 }
 
 func (dao gormDao) UpdateClientName(client Client, newName string) {
 	log.Println("Update client", client.Id, "name to", newName)
-	// logAffectedRows("Update client name", res)
+	client.Name = newName
+	result := dao.Save(&client)
+	if result.Error != nil {
+		log.Fatal(result.Error)
+	}
+	logAffectedRows("Update client name", result)
 }
 
 func (dao gormDao) DeleteCustomer(customer Customer) {
 	log.Println("Delete customer", customer.Id)
-	// logAffectedRows("Delete customer", res)
+	result := dao.Delete(&customer)
+	if result.Error != nil {
+		log.Fatal(result.Error)
+	}
+	logAffectedRows("Delete customer", result)
 }
 
 func (dao gormDao) DeleteAllCustomers() {
 	log.Println("Delete all customers")
-	// logAffectedRows("Delete all customers", res)
+	result := dao.Delete(Customer{})
+	logAffectedRows("Delete all customers", result)
 }
 
 func (dao gormDao) DeleteAllProducts() {
 	log.Println("Delete all products")
-	// logAffectedRows("Delete all products", res)
+	result := dao.Delete(Product{})
+	logAffectedRows("Delete all products", result)
 }
 
 func (dao gormDao) DeleteAllClients() {
 	log.Println("Delete all clients")
-	dao.db.Delete(Client{})
-	// logAffectedRows("Delete all clients", res)
+	result := dao.Delete(Client{})
+	logAffectedRows("Delete all clients", result)
 }
 
-func logAffectedRows(prefix string, dao gormDao) {
-	log.Printf("%-20s: %d row(s) affected", prefix, dao.db.RowsAffected)
+func logAffectedRows(prefix string, db *gorm.DB) {
+	log.Printf("%-20s: %d row(s) affected", prefix, db.RowsAffected)
 }
